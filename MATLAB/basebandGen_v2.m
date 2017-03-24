f9ffbe61daea375421fe4a376e5801bb4f9f5b6d
Hao <Hxu9@sheffield.ac.uk>
2017-03-24 01:27:40 +0000

    Filter update

--- MATLAB/basebandGen_v2.m
+++ MATLAB/basebandGen_v2.m
@@ -1,13 +1,14 @@
 %Baseband Generator
 fs = 1e6;%sampling rate
 t = 0:1/fs:0.1;%signal length
-fc1=9960;%secondary carrier
+
 ys = [];
 D = 480;%Deviation for the first carrier
 Dc = 5e3; %Deviation for the second carrier
 yc = [];
+fc1=1e4;%secondary carrier
 fc2=1e6;%Primary carrier frequency 1MHz
-fcam=9960;
+fcam=1e4;
 %%%%%%%%%IQ baseband%%%%%%%%
 idata = [];
 qdata = [];
@@ -20,7 +21,6 @@ for r = 1:length(t)
     % loop
     y(r) = sin(2*pi*30*t(r));
     ys(r) = cos(2*pi*(fc1+D*y(r)))*t(r); %subcarrier contain ref sig
-    
     %yc(r) = cos(2*pi*(fc2+Dc*ys(r))*t(r)); %carrier 
     idata(r)=sin(2*pi*fc1*t(r))*ys(r);
     qdata(r)=cos(2*pi*fc1*t(r))*ys(r);
@@ -29,26 +29,31 @@ for r = 1:length(t)
     %IQDatac(r)=sin(2*pi*yc(r))+j.*cos(2*pi*yc(r));
 end
 %%ys1 = y1.*ys;%frequency mixer 30Hz ref with 180 degree phase shift and ys
-
-ys2 = fmmod(y,fc1,fs,D);
-ysdm1=fmdemod(ys2,fc1,fs,D);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ys_normalized=ys/sqrt(sum(abs(ys.^2))/fs); 
 % IQdataLowpass=doFilter2(IQData);
+yofm = fmmod(y,fc1,fs,D);
+yofmd=fmdemod(yofm,fc1,fs,D);
 yd=sin(2*pi*30*t+degtorad(90));
-ydmodam=ammod(yd,fcam,fs);
-yddemodam=amdemod(ydmodam,fcam,fs);
-yfinal=ys2+ydmodam;
+ydam=ammod(yd,fcam,fs);
+ydamd=amdemod(ydam,fcam,fs);
+yfinal=yofm+ydam;
+yfinal=doFilter9000hz(yfinal);
 yfinalamdemod=amdemod(yfinal,fcam,fs);
- yfinalamdemodfilter=filter30hz(yfinalamdemod);
- yfinalfmdemodfilter=filter30hz(yfinalfmdemod);
-yfinalfmdemod=fmdemod(yfinal,9960,fs,480);
+yfinalfmdemod=fmdemod(yfinal,fc1,fs,480);
+yfinalamdemodfilter=filter30hz(yfinalamdemod);
+yfinalfmdemodfilter=filter30hz(yfinalfmdemod);
+
 figure(1)
 plot(t,yfinalamdemod,t,yfinalfmdemod)
 figure(2)
 plot(t,yd,t,y)
-figure(3)
-plot(t,yfinalamdemod,t,yfinalamdemodfilter);
+figure(6)
+plot(t,yd,t,y,t,yfinalamdemod,t,yfinalfmdemod)
+legend('yd','y','yam','yfm');
+% figure(3)
+% plot(t,yfinalamdemod,t,yfinalamdemodfilter);
 figure(5)
-plot(t,yfinalamdemodfilter,t,yfinalfmdemodfilter);
-% figure(4)
-% plot(t,abs(fft(yfinalamdemod)),xaxis,abs(fft(yfinalamdemodfilter)))
+plot(t,yfinalamdemod,t,yfinalamdemodfilter);
+figure(4)
+plot(t,abs(fft(yfinalamdemod)),xaxis,abs(fft(yfinalamdemodfilter)))
