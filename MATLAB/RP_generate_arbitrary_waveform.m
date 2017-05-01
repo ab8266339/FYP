8eb8c23b97e400d2d763c43c3ccf49b4e70ebb60
Hao-Pre <hxu9@sheffield.ac.uk>
2017-05-01 18:54:20 +0100

    4.30

--- MATLAB/RP_generate_arbitrary_waveform.m
+++ MATLAB/RP_generate_arbitrary_waveform.m
@@ -6,8 +6,8 @@ IP= '192.168.0.15';           % Input IP of your Red Pitaya...
 port = 5000;
 tcpipObj=tcpip(IP, port);
 
-tcpipObj.InputBufferSize = 32768*64;
-tcpipObj.OutputBufferSize = 32768*64;
+tcpipObj.InputBufferSize = 16384*64;
+tcpipObj.OutputBufferSize = 16384*64;
 flushinput(tcpipObj)
 flushoutput(tcpipObj)
 
@@ -20,12 +20,13 @@ tcpipObj.Terminator = 'CR/LF';
 %% Calcualte arbitrary waveform with 16384*2 samples
 % Values of arbitrary waveform must be in range from -1 to 1.
 
-N=32768;
+N=16383;
 t=0:(2*pi)/N:2*pi;
 bin = N/length(t);
 xaxis = 0:bin:bin*(length(t)-1);
-x=fmmod(sin(2*pi*30*t),1e4,5e4,480);
-y=ammod(sin(2*pi*30*t+degtorad(90)),1e4,5e4);
+x=fmmod(sin(2*pi*30*t),1000e3,125e6,480);
+%x=sin(t)+1/3*sin(3*t);
+y=ammod(sin(2*pi*300*t+degtorad(90)),200e3,125e6);
 plot(t,x,t,y)
 grid on
 
@@ -47,11 +48,11 @@ fprintf(tcpipObj,'SOUR2:FUNC ARBITRARY');       % {sine, square, triangle, sawu,
 fprintf(tcpipObj,['SOUR1:TRAC:DATA:DATA ' waveform_ch_1])  % Send waveforms to Red Pitya
 fprintf(tcpipObj,['SOUR2:TRAC:DATA:DATA ' waveform_ch_2])
 
-fprintf(tcpipObj,'SOUR1:VOLT 0.7');             % Set amplitude of output signal
+fprintf(tcpipObj,'SOUR1:VOLT 1');             % Set amplitude of output signal
 fprintf(tcpipObj,'SOUR2:VOLT 1');
 
-fprintf(tcpipObj,'SOUR1:FREQ:FIX 4000');        % Set frequency of output signal
-fprintf(tcpipObj,'SOUR2:FREQ:FIX 4000');
+fprintf(tcpipObj,'SOUR1:FREQ:FIX 1000e3');        % Set frequency of output signal
+fprintf(tcpipObj,'SOUR2:FREQ:FIX 200e3');
 
 
 fprintf(tcpipObj,'OUTPUT1:STATE ON');
