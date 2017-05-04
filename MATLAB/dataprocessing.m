81d1292dcd1e28ab11d18f73c1b7259fe84ee679
Hal <hxu9@sheffield.ac.uk>
2017-05-04 22:18:48 +0000

    1

--- MATLAB/dataprocessing.m
+++ MATLAB/dataprocessing.m
@@ -1,7 +1,8 @@
 %yam=amdemod(INNor,1e6,125e6);
 %yfm=fmdemod(INNor,1e6,125e6,480);
-yam=amdemod(INNor,1e6,s);
-yfm=fmdemod(INNor,1e6,s,480);
-% yamf=filter30hz125e6(yam);
-% yfmf=filter30hz125e6(yfm);
+s=125e6;
+yam=amdemod(INNor,1e6,125e6);
+yfm=fmdemod(INNor,1e6,125e6,480);
+yamf=filter30hz125e6(yam);
+yfmf=filter30hz125e6(yfm);
 
