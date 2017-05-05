8e6c14f3713bde235806010d61987a51447fd501
Hal <hxu9@sheffield.ac.uk>
2017-05-05 01:16:42 +0000

    p1

--- MATLAB/Prototype_1.m
+++ MATLAB/Prototype_1.m
@@ -1,8 +1,8 @@
 
 fcfm=9960;%secondary carrier used for fm
-fcam=1.54e9;
-fs = 4e9;%sampling rate
-t = 0:1/fs:0.01;%signal length
+fcam=108e6;
+fs = 1000e6;%sampling rate
+t = 0:1/fs:0.5;%signal length
 D = 480;%Deviation for the first carrier
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 bin = fs/length(t);
@@ -14,10 +14,13 @@ yofmdemod=fmdemod(yofm,fcfm,fs,D);
 yd=sin(2*pi*30*t+degtorad(180));
 ydam=ammod(yd,fcam,fs);
 ydamdemod=amdemod(ydam,fcam,fs);
-yfinal=yofm.*ydam;%yfinal is the final product after sinal mixing
+yfinal=yofm+ydam;%yfinal is the final product after sinal mixing
 
 
-figure(4)
-plot(xaxis,abs(fft(yfinal)),xaxis,abs(fft(ydam)));
+figure(1)
+plot(xaxis,abs(fft(yfinal)));
 title('fft');
 
+figure(2)
+plot(xaxis,abs(fft(ydam)));
+title('fft');
