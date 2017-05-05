fec7d3612794394b02f00455852d93c01408eb5c
Hao <Hxu9@sheffield.ac.uk>
2017-05-06 00:46:45 +0100

    angle

--- MATLAB/P2B.m
+++ MATLAB/P2B.m
@@ -1,7 +1,8 @@
-function [ yd ] = P2B( t,phase )
 %P2B Phase to Bearing
 %   sin(2*pi*phase*t)
-sin(2*pi*phase*t);
+function bearing = P2B(t,Phase)
+
+bearing = sin(2*pi*30*t+deg2rad(Phase));
 
 end
 
--- MATLAB/basebandGen_Angles.m
+++ MATLAB/basebandGen_Angles.m
@@ -12,32 +12,30 @@ xaxis = 0:bin:bin*(length(t)-1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 y = sin(2*pi*30*t);
 yofm = fmmod(y,fcfm,fs,D);% y is omnidirectional
-yofmdemod=fmdemod(yofm,fcfm,fs,D);
-yd=p2b(t,90);
-ydam=ammod(yd,fcam,fs);
-ydamdemod=amdemod(ydam,fcam,fs);
-yfinal=yofm+ydam;%yfinal is the final product after sinal mixing
-yfinalamdemod=amdemod(yfinal,fcam,fs);
-yfinalfmdemod=fmdemod(yfinal,fcfm,fs,D);
-yfinalamdemodfilter=filter30hz(yfinalamdemod);
-yfinalfmdemodfilter=filter30hz(yfinalfmdemod);
+phase=[0:45:360];
+for r=1:length(phase);
+    for r=phase(r);
+        yd=P2B(t,r);
+        ydam=ammod(yd,fcam,fs);
+        yfinal=yofm+ydam;%yfinal is the final product after sinal mixing
+        yfinalamdemod=amdemod(yfinal,fcam,fs);
+        yfinalfmdemod=fmdemod(yfinal,fcfm,fs,D);
+        yfinalamdemodfilter=filter30hz(yfinalamdemod);
+        yfinalfmdemodfilter=filter30hz(yfinalfmdemod);
+        figure()
+        plot(t,yd,t,y)
+        title(['y and ydirectional of ',num2str(r)]);
+        legend(['directional phase ',num2str(r)],'ref');
+        figure()
+        plot(t,yd,t,y,t,yfinalamdemodfilter,t,yfinalfmdemodfilter)
+        title(['yd y compare yamdemodfilter yfmdemodfilter with ' num2str(r) 'Phase Difference']);
+        legend(['yd of ',num2str(r)],'y','yamf','yfmf');
+        end
+end
+% figure(1)
+%         plot(t,yfinalamdemod,t,yfinalfmdemod);
+%         title('fmdemod and amdemod');
+%         legend('amdemod','fmdemod');
+% 
 
-figure(1)
-plot(t,yfinalamdemod,t,yfinalfmdemod);
-title('fmdemod and amdemod');
-legend('amdemod','fmdemod');
-figure(2)
-plot(t,yd,t,y)
-title('y and ydirectional');
-legend('directional','ref');
-% figure(3)
-% plot(t,yfinalamdemod,t,yfinalamdemodfilter);
-figure(5)
-plot(t,yfinalamdemod,t,yfinalamdemodfilter);
-title('yamdemod compare yamdemodfilter');
-legend('amdemod','amdemodfliter')
-figure(7)
-plot(t,yd,t,y,t,yfinalamdemodfilter,t,yfinalfmdemodfilter)
-title('yd y compare yamdemodfilter yfmdemodfilter');
-legend('yd','y','yamf','yfmf');
 
